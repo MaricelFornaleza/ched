34b2faf5872f40a3e84f5170c276bfc02edc808e
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2022-03-10 13:35:48 +0800

    install parse and tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import Parse from 'parse'
+Parse.initialize("cnmsp", "master");
+Parse.serverURL = 'http://localhost:1337/parse'
 
 createApp(App).mount('#app')
+
+new Parse.Object("Test", { "text": "testing" }).save()
+    .then(function (obj) {
+        console.log("Success", obj);
+    })
+    .catch(function (e) {
+        alert("Error saving test object!" + e.message);
+    });
