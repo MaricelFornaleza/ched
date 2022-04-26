2b9e8d5bf0f2999ad6548b4add5b5ef468c662e0
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2022-04-26 12:49:16 +0800

    Fix Summary datatable

--- src/partials/SummaryDataTable.vue
+++ src/partials/SummaryDataTable.vue
@@ -18,7 +18,7 @@
           <DownloadIcon class="h-5" />
         </button>
       </div>
-      <table id="dataTable" class="p-4 hover w-fit lg:w-full">
+      <table id="dataTable" width="100%" class="p-4 hover w-fit lg:w-full">
         <thead class="bg-gray-50 text-xs">
           <tr>
             <th rowspan="2" class="p-8 text-left">HEI Name</th>
@@ -123,11 +123,7 @@ import "jquery/dist/jquery.min.js";
 //Datatable Modules
 import "datatables.net-dt/js/dataTables.dataTables";
 import "datatables.net-dt/css/jquery.dataTables.min.css";
-import "datatables.net-buttons/js/dataTables.buttons.js";
-import "datatables.net-buttons/js/buttons.colVis.js";
-import "datatables.net-buttons/js/buttons.flash.js";
-import "datatables.net-buttons/js/buttons.html5.js";
-import "datatables.net-buttons/js/buttons.print.js";
+
 import $ from "jquery";
 import * as XLSX from "xlsx";
 import router from "../router";
@@ -176,6 +172,7 @@ export default {
           search: "",
           sLengthMenu: "_MENU_",
         },
+        scrollX: true,
       });
     });
   },
